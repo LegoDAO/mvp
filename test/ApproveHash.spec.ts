13804e0a89b626276f9844abb02850b185497cb9
jelle <jellegerbrandy@gmail.com>
2021-04-02 09:06:19 +0200

    commit test directory

---
+++ test/ApproveHash.spec.ts
@@ -0,0 +1,95 @@
+import { ethers, deployments, getNamedAccounts, getChainId } from "hardhat";
+import { expect } from "chai";
+import { Contract, Transaction } from "ethers";
+import {
+  safeAddOwner,
+  safeExecuteByOwner,
+  encodeParameters,
+} from "../scripts/utils";
+import { createAProposal } from "./utils";
+
+const STATE_PENDING = 0;
+const STATE_ACTIVE = 1;
+
+describe("Example DAO with Minime Token", () => {
+  let decisionEngine: Contract;
+  let safe: Contract;
+  let token: Contract;
+  let accounts: any;
+  let signers: any[];
+  let signer: any;
+  let fixture: any;
+  let NOOP_PROPOSAL: any;
+  let ProposalToMint10TokensToAddress1: any;
+
+  beforeEach(async () => {
+    fixture = await deployments.fixture(["ExampleDAOWithMiniMeToken"]);
+    decisionEngine = await ethers.getContractAt(
+      "DecisionEngine01",
+      fixture.DecisionEngine01.address
+    );
+    token = await ethers.getContractAt(
+      "MiniMeToken",
+      fixture.MiniMeToken.address
+    );
+    safe = await ethers.getContractAt("GnosisSafe", fixture.GnosisSafe.address);
+    accounts = await getNamedAccounts();
+    signers = await ethers.getSigners();
+    signer = signers[0];
+
+    // add a third owner, address1, and make it a 2/3 multisig
+    await safeAddOwner(safe, accounts.deployer, accounts.address1, 2);
+  });
+
+  it("approveHash ", async () => {
+    const { proposalId, tx } = await createAProposal(
+      decisionEngine,
+      token,
+      accounts.address1
+    );
+    let onChainProposalState;
+    let onChainProposal;
+
+    // lets inspect the proposal state
+    onChainProposal = await decisionEngine.proposals(proposalId);
+    expect(onChainProposal.id).to.equal(proposalId);
+    onChainProposalState = await decisionEngine.state(proposalId);
+    expect(onChainProposalState).to.equal(STATE_PENDING);
+
+    // proposal needs to be activated before voting, so we mine a block
+    ethers.provider.send("evm_mine", []);
+
+    // vote for the proposal
+    const VOTE_FOR = true;
+    await decisionEngine.castVote(proposalId, VOTE_FOR);
+    onChainProposalState = await decisionEngine.state(proposalId);
+    expect(onChainProposalState).to.equal(STATE_ACTIVE);
+    onChainProposal = await decisionEngine.proposals(proposalId);
+    expect(onChainProposal.forVotes).to.equal(
+      await token.balanceOf(accounts.deployer)
+    );
+
+    // wait for the period to end
+    for (let step = 0; step < 10; step += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await ethers.provider.send("evm_mine", []);
+    }
+
+    onChainProposalState = await decisionEngine.state(proposalId);
+
+    // execution of the proposal by the decisionEgine is expected to fail
+    // TODO: make this work
+    // const tx1 = await decisionEngine.execute(proposalId);
+    // no need to approve in this test because the DecisionEngine is the only owner in the Safe
+    // (if the DecisionEngine would only be one of the signers of the Safe, we would only approve the Hashh)
+
+    // await decisionEngine.approveHash(proposalId);
+
+    // expect(await token.balanceOf(accounts.address1)).to.equal(0);
+    // const receipt1 = await tx.wait();
+    // expect(receipt1.events[receipt1.events.length - 1].event).to.equal(
+    //   "ProposalExecuted"
+    // );
+    // expect(await token.balanceOf(accounts.address1)).to.equal(10);
+  });
+});
---
+++ test/utils.ts
@@ -0,0 +1,37 @@
+import { Contract, Transaction } from "ethers";
+import { encodeParameters } from "../scripts/utils";
+
+export async function createAProposal(
+  decisionEngine: Contract,
+  token: Contract,
+  to: string
+): Promise<{ tx: Transaction; proposalId: string }> {
+  let tx;
+  let receipt;
+
+  const proposalToMint10TokensToAddress1 = {
+    targets: [token.address],
+    values: ["0"],
+    signatures: ["generateTokens(address,uint256)"],
+    calldatas: [encodeParameters(["address", "uint"], [to, "10"])],
+  };
+
+  const {
+    targets,
+    values,
+    signatures,
+    calldatas,
+  } = proposalToMint10TokensToAddress1;
+
+  tx = await decisionEngine.propose(
+    targets,
+    values,
+    signatures,
+    calldatas,
+    "hello world"
+  );
+  receipt = await tx.wait();
+  const event = receipt.events[0];
+  const proposalId = event.args.id;
+  return { tx, proposalId };
+}
